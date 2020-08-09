2dc79d79ab479d31469046bbfcc73d1a53a3bb4f
Zenzi Ali <zeeathome935@gmail.com>
2020-08-09 17:19:05 -0400

    updated file path for github publishing

---
+++ appView.js
@@ -0,0 +1,15 @@
+
+const appView = `
+<section class="text-center-custom">
+  <header>
+    <h2>My Bookmarks</h2>
+  </header>
+  <main id="main-content">
+  </main>
+</section>
+`;
+
+
+export {appView};
+
+
